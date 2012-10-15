d57a64428586ae2e756f1e817c3f407b705325d4
Shawn Biddle <shawncplus@gmail.com>
2012-10-14 22:58:33 -0400

    I committed a syntax error.. good thing I'm working alone on this

--- frontend/lib/domus.js
+++ frontend/lib/domus.js
@@ -354,7 +354,7 @@ var Domus = {
 			app.use(express.bodyParser());
 			app.use(express.session({
 				secret: 'workingatwendys',
-				maxAge: 30 * 24 * 60 * 60 * 1000;
+				maxAge: 30 * 24 * 60 * 60 * 1000
 			}));
 			app.use(passport.initialize());
 			app.use(passport.session());
